5860f01db589b0903dd668797c0be57757e54157
huashihao123 <2632056402@qq.com>
2020-11-24 11:40:04 +0800

    第十八次课/js函数2.js

---
+++ 第十一周课程/第十八次课/js函数2.js
@@ -0,0 +1,74 @@
+
+
+// var apple=20;
+// var orange="橙子";
+
+// let lemno="柠檬";
+// let banana=30;
+// const pear=40;
+
+// var apple1={}
+// console.log( typeof apple1)
+
+// var apple2=Object.create(null);
+// console.log(typeof apple2+"apple2");
+
+// var apple3=function bigapple(){
+//     console.log("this is apple3 big apple");
+// }
+// function smallapple(){
+//     console.log("this is small apple");
+// }
+// apple3()
+// smallapple()
+
+// var appled4=function (){
+//     console.log("this is apple3 big apple");
+// }
+// appled4();
+
+// var apple5=()=>{
+//     console.log("this is arrow function");
+// }
+// apple5();
+
+// var apple6=()=>console.log("this is arrow function");
+
+// apple6();
+
+// var apple7=()=>{
+// return()=>{
+//     console.log("this is arrow function apple7");
+//     }
+//  }
+// apple7()();
+
+// var apple10=(x,y)=>{
+//     console.log("this is apple10==" +(x+y));
+//     return (x,y)=>{
+//         console.log("this is apple11=="+(x+y));
+//     }
+// }
+// //NaN not a number
+// apple10(10,10)(4,5);
+
+// var apple12=()=>console.log(10);console.log(13);
+
+// function apple13(){
+//     var name="苹果13";
+//     function apple135on(){
+//         console.log("this is apple13 son")
+//     }
+//     apple135on();
+//     console.log("this is apple13");
+//     return{
+//         text:apple135on
+//     }
+// }
+// apple13().text();
+
+ var apple14=function(){
+     console.log("this is apple14....")
+     return 100;
+ }()
+ console.log(apple14);
